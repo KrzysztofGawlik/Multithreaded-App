65f8b1173efa9c629147e94e7b8bd432c6a79ef9
KrzysztofGawlik <krzysztof.gaw@outlook.com>
2023-06-10 14:44:47 +0200

    Added logging to console

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -1,8 +1,11 @@
+#include <iostream>
+
 #include "Logger.h"
 
 void Logger::log(std::string logMessage)
 {
     buffer.push_back(logMessage);
+    std::cout << logMessage << std::endl;
 }
 
 std::vector<std::string> Logger::getLogs()
